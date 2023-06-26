4e93ba01c0e7342d1d13fde5aa5701ecb92394c2
DoBeJamin <benjaminick11@gmail.com>
2023-06-26 16:45:42 -0500

    finished light blinking program

---
+++ src/Lab1.cpp
@@ -0,0 +1,30 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "c:/Users/benja/OneDrive/Desktop/visualStudioCode/summerIOT/Lab1/src/Lab1.ino"
+
+
+
+void setup();
+void loop();
+#line 4 "c:/Users/benja/OneDrive/Desktop/visualStudioCode/summerIOT/Lab1/src/Lab1.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+  pinMode(D5,OUTPUT);
+  delay(1000);
+  digitalWrite(D5, HIGH);
+  delay(1000);
+  digitalWrite(D5, LOW);
+}
--- src/Lab1.ino
+++ src/Lab1.ino
@@ -1,9 +1,8 @@
-/*
- * Project Lab1
- * Description:
- * Author:
- * Date:
- */
+
+
+
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
 
 // setup() runs once, when the device is first turned on.
 void setup() {
@@ -14,5 +13,9 @@ void setup() {
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
   // The core of your code will likely live here.
-
+  pinMode(D5,OUTPUT);
+  delay(1000);
+  digitalWrite(D5, HIGH);
+  delay(1000);
+  digitalWrite(D5, LOW);
 }
